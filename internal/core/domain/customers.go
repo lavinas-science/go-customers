a6b156395bb7d31104276ed7db755a7d7ac3c77b
lavinas <lavinas@me.com>
2021-11-12 16:05:42 -0300

    cpf validation

--- internal/core/domain/customers.go
+++ internal/core/domain/customers.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	env_validate_document = "REQUIRE_DOCUMENT"
-	cpf_max_length        = 11
+	cpf_length        = 11
 )
 
 // Customer represents basic informations of a Costumer
@@ -60,10 +60,17 @@ func (c Customer) IsDocumentCPF() bool {
 		return false
 	}
 	// valid digit 1
-	val := 0
-	for i := 3; i <= cpf_max_length; i++ {
+	dig1 := int(c.Document%100/10)
+	dig2 := int(c.Document%10)
+	val1 := 0
+	val2 := 0
+	for i := 3; i <= cpf_length; i++ {
 		x := int(math.Mod(float64(c.Document), math.Pow10(i)) / math.Pow10(i-1))
-		val += x * (i - 1)
+		val1 += x * (i - 1)
+		val2 += x * i
 	}
-	return int(math.Mod(float64(val*10), float64(11))) == int(c.Document%100/10)
+	val2 += dig1 * 2
+	val1 = int(math.Mod(float64(val1*10), float64(11)))
+	val2 = int(math.Mod(float64(val2*10), float64(11)))
+	return val1 == dig1 && val2 == dig2
 }
--- internal/core/domain/custumers_test.go
+++ internal/core/domain/custumers_test.go
@@ -5,8 +5,19 @@ import (
 	"testing"
 )
 
+func TestConstant(t *testing.T) {
+	assert.Equal(t, env_validate_document, "REQUIRE_DOCUMENT")
+	assert.Equal(t, cpf_length, 11)
+}
+
 func TestIsDocumentCPF(t *testing.T) {
-	var c = Customer{Document: 4417932834}
+	var c = Customer{Document: 66946202848}
 	x := c.IsDocumentCPF()
 	assert.True(t, x)
+	c = Customer{Document: 66946202818}
+	x = c.IsDocumentCPF()
+	assert.False(t, x)
+	c = Customer{Document: 66946202840}
+	x = c.IsDocumentCPF()
+	assert.False(t, x)
 }
